9bc60c7044fd91d008f4ecbcb3c7afb9cff03bd2
devalpha18 <devalpha18@gmail.com>
2024-06-27 02:31:31 -0300

    fix: file pat

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -8,7 +8,7 @@ import {
   setClearCartItems,
   setCloseCart,
   setGetTotals,
-} from "../app/CartSlice.js";
+} from "../../src/app/CartSlice.js";
 import CartCount from "./cart/CartCount";
 import CartEmpty from "./cart/CartEmpty";
 import CartItem from "./cart/CartItem";
